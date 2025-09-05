f5a328e3e028ab6f9438a52e1fddce79d384cb15
Luis Mario <luismario337y@gmail.com>
2025-09-05 16:15:47 -0300

    upd: update database

--- api/src/databases/DataBaseConnection.js
+++ api/src/databases/DataBaseConnection.js
@@ -24,3 +24,4 @@ export const query = (query, values = "", mensage, options) => {
     }
 }
 
+_DATABASE.prepare("CREATE TABLE shopping_list_items(id INTEGER PRIMARY KEY AUTOINCREMENT, listID INTEGER NOT NULL, productID INTEGER NOT NULL, quantity INTEGER DEFAULT 1, FOREIGN KEY (listID) REFERENCES shopping_list_items(id) ON DELETE CASCADE, FOREIGN KEY (productID) REFERENCES products(id) ON DELETE CASCADE);").run();
